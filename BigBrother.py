967ab4e4a0665ae2b49bfa2b73df9bcd6e576c74
icbmicbm <icbmicbm@yandex.com>
2020-02-23 16:53:40 +0800

    comment added

--- BigBrother.py
+++ BigBrother.py
@@ -5,13 +5,19 @@ import time
 import json
 
 '''
-libBigBrother version 0.0.1
+libBigBrother version 1.0.0
 By icbmicbm
 libBigBrother is the library needed by Project BigBrother.
 '''
 
 
 def getFile(path, rlist):
+    """
+    获取单个目录下的文件，此函数仅被getFileList调用
+    :param path: 路径
+    :param rlist: 未完成的文件列表
+    :return: 返回一个文件列表
+    """
     files = os.listdir(path)
     for file in files:
         if os.path.isdir(os.path.join(path, file)):
@@ -22,12 +28,22 @@ def getFile(path, rlist):
 
 
 def getFileList(path):
+    """
+    递归地获取一路径下的所有文件
+    :param path: 需要扫描的路径
+    :return: 该路径下所有文件的列表
+    """
     rlist = []
     getFile(path, rlist)
     return rlist
 
 
 def md5Sum(file):
+    """
+    计算一个文件的md5值
+    :param file: 需要处理的文件
+    :return: 返回一个文件的md5值
+    """
     fp = open(file, 'rb')
     content = fp.read()
     fp.close()
@@ -37,6 +53,11 @@ def md5Sum(file):
 
 
 def getmd5List(rlist):
+    """
+    计算一个rlist内所有文件的md5
+    :param rlist: 文件列表
+    :return: md5列表
+    """
     md5list = {}
     for i in rlist:
         md5list[i] = md5Sum(i)
@@ -44,6 +65,11 @@ def getmd5List(rlist):
 
 
 def savemd5List(md5list):
+    """
+    储存一个md5list
+    :param md5list: 需要储存的md5list
+    :return: 无返回值
+    """
     dump = json.dumps(md5list)
     try:
         md5file = open(".md5list.txt", "a")
@@ -54,6 +80,11 @@ def savemd5List(md5list):
 
 
 def readmd5List(file):
+    """
+    读取一个md5列表
+    :param file: md5列表路径
+    :return: 返回读取的md5列表
+    """
     try:
         md5list = open(file, "r")
         md5list = json.loads(md5list)
@@ -63,6 +94,12 @@ def readmd5List(file):
 
 
 def getCompressed(rlist, outpath):
+    """
+    压缩rlist内的所有文件
+    :param rlist: 文件列表
+    :param outpath: 压缩文件储存位置
+    :return: 储存路径
+    """
     for i in rlist:
         i = os.path.basename(i)
     filepath = outpath + str(time.time()) + ".tar.gz"
@@ -77,6 +114,7 @@ def getCompressed(rlist, outpath):
     return filepath
 
 
+# TODO need rework
 def md5Check(md5list, path):
     rlist = []
     checklist = getFileList(path)
@@ -91,6 +129,12 @@ def md5Check(md5list, path):
 
 
 def getExtracted(file, outpath):
+    """
+    解压一个压缩文件
+    :param file: 压缩文件
+    :param outpath: 输出路径
+    :return: 无返回值
+    """
     compressed = tarfile.open(file)
     try:
         compressed.extractall(path=outpath)
@@ -99,6 +143,12 @@ def getExtracted(file, outpath):
 
 
 def deleteNewFile(md5list, rlist):
+    """
+    删除不在md5list中的文件
+    :param md5list: 已计算的md5list
+    :param rlist: 当前的所有文件列表
+    :return: 被删除文件列表
+    """
     deleted = []
     for i in rlist:
         if i not in md5list.keys():
@@ -108,10 +158,17 @@ def deleteNewFile(md5list, rlist):
 
 
 def accurateRecovery(rlist, inpath, outpath):
+    """
+    从备份恢复文件
+    :param rlist: 需要还原的文件
+    :param inpath: 备份文件路径
+    :param outpath: 被还原文件路径
+    :return: 无返回值
+    """
     inlist = getFileList(inpath)
     outlist = getFileList(outpath)
     for i in range(len(rlist)):
-        rlist[i] = os.path.join(inpath,rlist[i][1:])
+        rlist[i] = os.path.join(inpath, rlist[i][1:])
     for j in range(min(len(inlist), len(outlist))):
         if inlist[j] in rlist:
-            os.replace(inlist[j], os.path.join(outpath,os.path.basename(inlist[j])))
+            os.replace(inlist[j], os.path.join(outpath, os.path.basename(inlist[j])))
