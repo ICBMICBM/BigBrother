3a7647fee92039366105dfef7e011d5299bc5378
icbmicbm <icbmicbm@yandex.com>
2020-08-17 21:04:11 +0800

    test and moving function from BB1 to BB2

--- BigBrother2.py
+++ BigBrother2.py
@@ -1,6 +1,9 @@
 import os
 import plyvel
 import hashlib
+import json
+import tarfile
+import time
 
 '''
 libBigBrother version 2.0.0
@@ -8,6 +11,7 @@ By icbmicbm
 libBigBrother is the library needed by Project BigBrother.
 '''
 
+# tested
 def getFileList(path: str):
     fileList = []
     for dirPath,dirNames,fileNames in os.walk(path):
@@ -15,15 +19,18 @@ def getFileList(path: str):
             fileList.append(os.path.join(dirPath,f))
     return fileList
 
-
+# not tested
 def saveMD5(host: str,fileList: list):
     db = plyvel.DB(host, create_if_missing=True)
     with db.write_batch() as wb:
         for i in fileList:
-            wb.put(i,md5Sum(i))
+            key = i.encode(encoding="utf-8")
+            value = json.dumps({"sum":md5Sum(i),"locked":True}).encode(encoding="utf-8")
+            wb.put(key,value)
     wb.write()
     db.close()
 
+# tested
 def md5Sum(file):
     """
     计算一个文件的md5值
@@ -36,3 +43,70 @@ def md5Sum(file):
     m = hashlib.md5(content)
     file_md5 = m.hexdigest()
     return file_md5
+
+# tested
+def getCompressed(fileList, outpath):
+    """
+    压缩fileList内的所有文件
+    :param fileList: 文件列表
+    :param outpath: 压缩文件储存位置
+    :return: 储存路径
+    """
+    for i in fileList:
+        i = os.path.basename(i)
+    filepath = os.path.join(outpath,str(time.time()) + ".tar.gz")
+    compressed = tarfile.open(filepath, "x:gz")
+    for i in fileList:
+        try:
+            compressed.add(i)
+        except Exception as e:
+            print(e)
+            break
+    compressed.close()
+    return filepath
+
+# tested
+def md5check(host: str,fileList: list):
+    missing = []
+    toremove = []
+    toreplace = []
+    db = plyvel.DB(host, create_if_missing=False)
+    oldfile = db.iterator(include_value=False)
+    for j in oldfile:
+        j = str(j,encoding="utf-8")
+        if j not in fileList:
+            missing.append(j)
+    for i in fileList:
+        nowsum = md5Sum(i)
+        oldsum = db.get(i.encode(encoding="utf-8"))
+        if oldsum is None:
+            toremove.append(i)
+        else:
+            oldsum = json.loads(str(oldsum,encoding="utf-8"))["sum"]
+            print(i," ",nowsum," ",oldsum)
+            if nowsum != oldsum:
+                print("appending",i)
+                toreplace.append(i)
+    db.close()
+    return missing, toremove, toreplace
+
+# not tested
+def getCompressed(rlist, outpath):
+    """
+    压缩rlist内的所有文件
+    :param rlist: 文件列表
+    :param outpath: 压缩文件储存位置
+    :return: 储存路径
+    """
+    for i in rlist:
+        i = os.path.basename(i)
+    filepath = os.path.join(outpath,str(time.time()) + ".tar.gz")
+    compressed = tarfile.open(filepath, "x:gz")
+    for i in rlist:
+        try:
+            compressed.add(i)
+        except Exception as e:
+            print(e)
+            break
+    compressed.close()
+    return filepath
--- Tester.py
+++ Tester.py
@@ -1,9 +1,12 @@
 import BigBrother2
 import unittest
+import os
 
 
 class MyTestCase(unittest.TestCase):
     testPath = "/Users/icbm/Desktop/TestFolder"
+    testOutPath = "/Users/icbm/Desktop/TestOutPath"
+    outPath = "/Users/icbm/Desktop"
     DBPath = "./testdb"
 
     def test1(self):
@@ -11,6 +14,26 @@ class MyTestCase(unittest.TestCase):
 
     def test2(self):
         BigBrother2.connectDB(self.DBPath)
+
+    def test3(self):
+        BigBrother2.saveMD5(self.DBPath,BigBrother2.getFileList(self.testPath))
+        missing, toremove, toreplace = BigBrother2.md5check(self.DBPath,BigBrother2.getFileList(self.testPath))
+        print(missing, toremove, toreplace)
+
+    def test4(self):
+        files = BigBrother2.getFileList(self.testPath)
+        BigBrother2.getCompressed(files,self.outPath)
+
+    def test5(self):
+        BigBrother2.saveMD5(self.DBPath, BigBrother2.getFileList(self.testPath))
+        with open(os.path.join("/Users/icbm/Desktop/TestFolder/a.txt"),"w") as f:
+            f.write("appending")
+            f.close()
+        missing, toremove, toreplace = BigBrother2.md5check(self.DBPath, BigBrother2.getFileList(self.testPath))
+        print(missing, toremove, toreplace)
+
+    def test6(self):
+        print(BigBrother2.getCompressed(BigBrother2.getFileList(self.testPath),self.testOutPath))
 
 
 if __name__ == '__main__':
